7893179b7f526a8d8031455ff90982d13bc2a612
mauroprojetos <mauroprojetos@gmail.com>
2023-04-22 15:01:11 -0300

    Código de exemplo

---
+++ brand.conf.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'BRAND_LOGO' => '/imgstore.php?iconid=233',
+    'BRAND_LOGO_SIDEBAR' => '/imgstore.php?iconid=235',
+    'BRAND_LOGO_SIDEBAR_COMPACT' => '/imgstore.php?iconid=234',
+    'BRAND_FOOTER' => '©NOC GRANITO',
+    'BRAND_HELP_URL' => 'https://www.example.com/help/'
+];
